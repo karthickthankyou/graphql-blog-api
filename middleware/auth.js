b500282d65ff9376e2b8c742886b225cf68afcce
Karthick Ragavendran <karthickragavendran@Karthicks-MacBook-Air.local>
2020-09-12 01:25:26 +0530

    Created auth util and middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken")
+
+const authenticate = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1] || ""
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    // const decoded = jwt.decode(token, process.env.JWT_SECRET)
+    req.verifiedUser = verified
+    console.log("Verification success!", verified)
+    next()
+  } catch (err) {
+    console.log("Verification failed!", err)
+    next()
+  }
+}
+
+module.exports = { authenticate }
---
+++ util/auth.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken")
+
+const createJwtToken = (user) => {
+  return jwt.sign(user, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  })
+}
+
+module.exports = { createJwtToken }
